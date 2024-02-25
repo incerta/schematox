efe1f4af3b5171f4ee88403c8978b12c236951be
Konstantin Mazur <incerta.dei@gmail.com>
2024-02-25 09:02:16 +0100

    Reorganize constructors tests and correct docs

--- src/test/test-utils.test.ts
+++ src/test/test-utils.test.ts
@@ -1,43 +1,183 @@
 import { check, unknownX } from './test-utils'
 
-it('check: type compatibility', () => {
-  type A = string
-  type B = string
+describe('Union of literals', () => {
+  it('Valid case', () => {
+    type Expected = 'x' | 'y'
+    type Actual = 'x' | 'y'
 
-  check<A, B>
-  // @ts-expect-error always
-  check<never, B>
+    check<Expected, Actual>()
+    check<Actual, Expected>()
 
-  // We require `never` cast for the cases when B might infer `never`
-  // `never` is the type that can be extended by itself only
-  check<never, never>
+    check<Actual>(unknownX as Expected)
+    check<Expected>(unknownX as Actual)
+  })
 
-  type C = string
-  type D = number
+  it('Actual union has extra member', () => {
+    type Expected = 'x' | 'y'
+    type Actual = 'x' | 'y' | 'z'
 
-  // @ts-expect-error C != D
-  check<C, D>
+    // @ts-expect-error '"z"' is not assignable to type 'Expected'
+    check<Expected, Actual>()
+    check<Actual, Expected>()
+
+    // @ts-expect-error 'Actual' is not assignable to parameter of type 'Expected'
+    check<Expected>(unknownX as Actual)
+    check<Actual>(unknownX as Expected)
+  })
+
+  it('Actual union has "never" that replaced one of its members', () => {
+    type Expected = 'x' | 'y'
+    type Actual = 'x' | never
+
+    check<Expected, Actual>()
+    // @ts-expect-error 'Expected' does not satisfy the constraint '"x"'
+    check<Actual, Expected>()
+
+    check<Expected>(unknownX as Actual)
+    // @ts-expect-error 'Expected' does not satisfy the constraint '"x"'
+    check<Actual>(unknownX as Expected)
+  })
+
+  it('Actual is "never"', () => {
+    type Expected = 'x' | 'y'
+    type Actual = 'x' | never
+
+    check<Expected, Actual>()
+    // @ts-expect-error 'Expected' does not satisfy the constraint '"x"'
+    check<Actual, Expected>()
+
+    check<Expected>(unknownX as Actual)
+    // @ts-expect-error 'Expected' does not satisfy the constraint '"x"'
+    check<Actual>(unknownX as Expected)
+  })
 })
 
-it('check: type of declared identifier', () => {
-  const x = 'x'
+describe('Array of primitives', () => {
+  it('Valid case', () => {
+    type Expected = Array<string | number>
+    type Actual = Array<string | number>
 
-  check<string>(x)
-  // @ts-expect-error always
-  check<never>(x)
+    check<Expected, Actual>()
+    check<Actual, Expected>()
 
-  const y = 0
+    check<Expected>(unknownX as Actual)
+    check<Actual>(unknownX as Expected)
+  })
 
-  check<number>(y)
-  // @ts-expect-error always
-  check<never>(y)
+  it('Actual array elements has one extra member', () => {
+    type Expected = Array<string | number>
+    type Actual = Array<string | number | boolean>
+
+    // @ts-expect-error 'string | number | boolean' is not assignable to type 'string | number'
+    check<Expected, Actual>()
+    check<Actual, Expected>()
+
+    // @ts-expect-error 'string | number | boolean' is not assignable to type 'string | number'
+    check<Expected>(unknownX as Actual)
+    check<Actual>(unknownX as Expected)
+  })
+
+  it('Actual array elements han one member replaced by "never"', () => {
+    type Expected = Array<string | number>
+    type Actual = Array<string | never>
+
+    check<Expected, Actual>()
+    // @ts-expect-error 'Expected' does not satisfy the constraint 'string[]'
+    check<Actual, Expected>()
+
+    check<Expected>(unknownX as Actual)
+    // @ts-expect-error 'Expected' does not satisfy the constraint 'string[]'
+    check<Actual>(unknownX as Expected)
+  })
+
+  it('Actual array elements is "never"', () => {
+    type Expected = Array<string | number>
+    type Actual = Array<never>
+
+    check<Expected, Actual>()
+    // @ts-expect-error 'Expected' does not satisfy the constraint 'never[]'
+    check<Actual, Expected>()
+
+    check<Expected>(unknownX as Actual)
+    // @ts-expect-error 'Expected' does not satisfy the constraint 'never[]'
+    check<Actual>(unknownX as Expected)
+  })
+
+  it('Actual array elements is "never"', () => {
+    type Expected = Array<string | number>
+    type Actual = Array<never>
+
+    check<Expected, Actual>()
+    // @ts-expect-error 'Expected' does not satisfy the constraint 'never[]'
+    check<Actual, Expected>()
+
+    check<Expected>(unknownX as Actual)
+    // @ts-expect-error 'Expected' does not satisfy the constraint 'never[]'
+    check<Actual>(unknownX as Expected)
+  })
+
+  it('Actual "never"', () => {
+    type Expected = Array<string | number>
+    type Actual = never
+
+    check<Expected, Actual>()
+    // @ts-expect-error 'Expected' does not satisfy the constraint 'never'
+    check<Actual, Expected>()
+
+    check<Expected>(unknownX as Actual)
+    // @ts-expect-error 'Expected' does not satisfy the constraint 'never'
+    check<Actual>(unknownX as Expected)
+  })
 })
 
-it('check: should return given value', () => {
-  expect(check('x')).toStrictEqual(['x'])
-  expect(check('y')).toStrictEqual(['y'])
-})
+describe('Object of primitive props', () => {
+  it('Valid case', () => {
+    type Expected = { x: string; y: number }
+    type Actual = { x: string; y: number }
 
-it('unknownX: value has undefined type', () => {
-  expect(unknownX).toBe(undefined)
+    check<Expected, Actual>()
+    check<Actual, Expected>()
+
+    check<Expected>(unknownX as Actual)
+    check<Actual>(unknownX as Expected)
+  })
+
+  it('Actual has extra property', () => {
+    type Expected = { x: string; y: number }
+    type Actual = { x: string; y: number; z: boolean }
+
+    check<Expected, Actual>()
+    // @ts-expect-error Property 'z' is missing in type 'Expected' but required in type 'Actual'
+    check<Actual, Expected>()
+
+    check<Expected>(unknownX as Actual)
+    // @ts-expect-error Property 'z' is missing in type 'Expected' but required in type 'Actual'
+    check<Actual>(unknownX as Expected)
+  })
+
+  it('One of actual properties has "never" value type', () => {
+    type Expected = { x: string; y: number }
+    type Actual = { x: string; y: never }
+
+    check<Expected, Actual>()
+    // @ts-expect-error Types of property 'y' are incompatible.    Type 'number' is not assignable to type 'never'.
+    check<Actual, Expected>()
+
+    check<Expected>(unknownX as Actual)
+    // @ts-expect-error Types of property 'y' are incompatible.    Type 'number' is not assignable to type 'never'.
+    check<Actual>(unknownX as Expected)
+  })
+
+  it('All actual properties has "never" value type', () => {
+    type Expected = { x: string; y: number }
+    type Actual = { x: never; y: never }
+
+    check<Expected, Actual>()
+    // @ts-expect-error 'Expected' does not satisfy the constraint 'Actual'
+    check<Actual, Expected>()
+
+    check<Expected>(unknownX as Actual)
+    // @ts-expect-error 'Expected' does not satisfy the constraint 'Actual'
+    check<Actual>(unknownX as Expected)
+  })
 })
--- src/test/types/extensions.test.ts
+++ src/test/types/extensions.test.ts
@@ -1,237 +1,118 @@
 import { check } from '../test-utils'
-import {
-  ExtWith_Undefined_SubjT,
-  ExtWith_Null_SubjT,
-  ExtWith_Brand_SubjT,
-  ExtWith_SchemaParams_SubjT,
-} from '../../types/extensions'
+import { ExtWith_SchemaParams_SubjT } from '../../types/extensions'
 
-describe('U is primitive', () => {
+describe('ExtWith_SchemaParams_SubjT<T, U>: U is primitive', () => {
   type U = string
 
-  it('ExtWith_Undefined_SubjT<T, U>: extend U with `undefined` based on schema T', () => {
-    type NotExt = ExtWith_Undefined_SubjT<null, U>
+  it('Extend nothing if parameters are not specified', () => {
+    const schema = {} as const
 
-    check<U, NotExt>()
-    // @ts-expect-error always
-    check<never, NotExt>()
+    type Expected = U
+    type Actual = ExtWith_SchemaParams_SubjT<typeof schema, U>
 
-    type Ext = ExtWith_Undefined_SubjT<{ optional: true }, U>
-
-    check<U | undefined, Ext>()
-    // @ts-expect-error U != U | undefined
-    check<U, Ext>()
+    check<Expected, Actual>()
+    check<Actual, Expected>()
   })
 
-  it('ExtWith_Null_SubjT<T, U>: extend U with `null` based on schema T', () => {
-    type NotExt = ExtWith_Null_SubjT<null, U>
+  it('Optional', () => {
+    const schema = { optional: true } as const
 
-    check<U, NotExt>
-    // @ts-expect-error always
-    check<never, NotExt>
+    type Expected = U | undefined
+    type Actual = ExtWith_SchemaParams_SubjT<typeof schema, U>
 
-    type Ext = ExtWith_Null_SubjT<{ nullable: true }, U>
-
-    check<U | null, Ext>
-    // @ts-expect-error U != U | null
-    check<U, Ext>
+    check<Actual, Expected>()
+    check<Expected, Actual>()
   })
 
-  it('ExtWith_Brand_SubjT<T, U>: extend U with brand based on schema T', () => {
-    type NotExt = ExtWith_Brand_SubjT<null, U>
+  it('Nullable', () => {
+    const schema = { nullable: true } as const
 
-    check<U, NotExt>
-    // @ts-expect-error always
-    check<never, NotExt>
+    type Expected = U | null
+    type Actual = ExtWith_SchemaParams_SubjT<typeof schema, U>
 
-    type Ext = ExtWith_Brand_SubjT<{ brand: Readonly<['x', 'y']> }, U>
-
-    check<U & { __x: 'y' }, Ext>
-    // @ts-expect-error always
-    check<never, Ext>
+    check<Actual, Expected>()
+    check<Expected, Actual>()
   })
 
-  describe('ExtWith_SchemaParams_SubjT<T, U>', () => {
-    it('ExtWith_SchemaParams_SubjT<T, U>: undefined -> null -> brand', () => {
-      type NotExt = ExtWith_SchemaParams_SubjT<null, U>
+  it('Branded', () => {
+    const schema = { brand: ['x', 'y'] } as const
 
-      check<U, NotExt>
-      // @ts-expect-error always
-      check<never, NotExt>
+    type Expected = U & { __x: 'y' }
+    type Actual = ExtWith_SchemaParams_SubjT<typeof schema, U>
 
-      type ExtUnd = ExtWith_SchemaParams_SubjT<{ optional: true }, U>
+    check<Actual, Expected>()
+    check<Expected, Actual>()
+  })
 
-      check<U | undefined, ExtUnd>
-      // @ts-expect-error U != U | undefined
-      check<U, ExtUnd>
+  it('Optional + nullable + branded', () => {
+    const schema = {
+      optional: true,
+      nullable: true,
+      brand: ['x', 'y'],
+    } as const
 
-      type ExtUndNull = ExtWith_SchemaParams_SubjT<
-        { optional: true; nullable: true },
-        U
-      >
+    type Expected = (U & { __x: 'y' }) | null | undefined
+    type Actual = ExtWith_SchemaParams_SubjT<typeof schema, U>
 
-      check<U | undefined | null, ExtUndNull>
-      // @ts-expect-error U | undefined != U | undefined | null
-      check<U | undefined, ExtUndNull>
-
-      type ExtUndNullBrand = ExtWith_SchemaParams_SubjT<
-        { optional: true; nullable: true; brand: Readonly<['x', 'y']> },
-        U
-      >
-
-      check<undefined | null | (U & { __x: 'y' }), ExtUndNullBrand>
-      // @ts-expect-error undefined | null != undefined | null | (U & { __x: 'y' }
-      check<undefined | null, ExtUndNullBrand>
-    })
-
-    it('ExtWith_SchemaParams_SubjT<T, U>: brand -> null -> undefined', () => {
-      type NotExt = ExtWith_SchemaParams_SubjT<null, U>
-
-      check<U, NotExt>
-      // @ts-expect-error always
-      check<never, NotExt>
-
-      type ExtBrand = ExtWith_SchemaParams_SubjT<
-        { brand: Readonly<['x', 'y']> },
-        U
-      >
-
-      check<U & { __x: 'y' }, ExtBrand>
-      // @ts-expect-error always
-      check<never, ExtBrand>
-
-      type ExtBrandNull = ExtWith_SchemaParams_SubjT<
-        { brand: Readonly<['x', 'y']>; nullable: true },
-        U
-      >
-
-      check<(U & { __x: 'y' }) | null, ExtBrandNull>
-      // @ts-expect-error U & { __x: 'y' } != U & { __x: 'y' }) | null
-      check<U & { __x: 'y' }, ExtBrandNull>
-
-      type ExtBrandNullUndef = ExtWith_SchemaParams_SubjT<
-        { brand: Readonly<['x', 'y']>; nullable: true; optional: true },
-        U
-      >
-
-      check<(U & { __x: 'y' }) | null | undefined, ExtBrandNullUndef>
-      // @ts-expect-error U & { __x: 'y' } | null != U & { __x: 'y' }) | null | undefined
-      check<(U & { __x: 'y' }) | null, ExtBrandNullUndef>
-    })
+    check<Actual, Expected>()
+    check<Expected, Actual>()
   })
 })
 
-describe('U is object', () => {
-  type U = { x: number; y: string }
+describe('ExtWith_SchemaParams_SubjT<T, U>: U is object', () => {
+  type U = { x: string; y: number }
 
-  it('ExtWith_Undefined_SubjT<T, U>: extend U with `undefined` based on schema T', () => {
-    type NotExt = ExtWith_Undefined_SubjT<null, U>
+  it('Extend nothing if parameters are not specified', () => {
+    const schema = {} as const
 
-    check<U, NotExt>()
-    // @ts-expect-error always
-    check<never, NotExt>()
+    type Expected = U
+    type Actual = ExtWith_SchemaParams_SubjT<typeof schema, U>
 
-    type Ext = ExtWith_Undefined_SubjT<{ optional: true }, U>
-
-    check<U | undefined, Ext>()
-    // @ts-expect-error U != U | undefined
-    check<U, Ext>()
+    check<Actual, Expected>()
+    check<Expected, Actual>()
   })
 
-  it('ExtWith_Null_SubjT<T, U>: extend U with `null` based on schema T', () => {
-    type NotExt = ExtWith_Null_SubjT<null, U>
+  it('Optional', () => {
+    const schema = { optional: true } as const
 
-    check<U, NotExt>
-    // @ts-expect-error always
-    check<never, NotExt>
+    type Expected = U | undefined
+    type Actual = ExtWith_SchemaParams_SubjT<typeof schema, U>
 
-    type Ext = ExtWith_Null_SubjT<{ nullable: true }, U>
-
-    check<U | null, Ext>
-    // @ts-expect-error U != U | null
-    check<U, Ext>
+    check<Actual, Expected>()
+    check<Expected, Actual>()
   })
 
-  it('ExtWith_Brand_SubjT<T, U>: extend U with brand based on schema T', () => {
-    type NotExt = ExtWith_Brand_SubjT<null, U>
+  it('Nullable', () => {
+    const schema = { nullable: true } as const
 
-    check<U, NotExt>
-    // @ts-expect-error always
-    check<never, NotExt>
+    type Expected = U | null
+    type Actual = ExtWith_SchemaParams_SubjT<typeof schema, U>
 
-    type Ext = ExtWith_Brand_SubjT<{ brand: Readonly<['x', 'y']> }, U>
-
-    check<U & { __x: 'y' }, Ext>
-    // @ts-expect-error always
-    check<never, Ext>
+    check<Actual, Expected>()
+    check<Expected, Actual>()
   })
 
-  describe('ExtWith_SchemaParams_SubjT<T, U>', () => {
-    it('ExtWith_SchemaParams_SubjT<T, U>: undefined -> null -> brand', () => {
-      type NotExt = ExtWith_SchemaParams_SubjT<null, U>
+  it('Branded', () => {
+    const schema = { brand: ['x', 'y'] } as const
 
-      check<U, NotExt>
-      // @ts-expect-error always
-      check<never, NotExt>
+    type Expected = U & { __x: 'y' }
+    type Actual = ExtWith_SchemaParams_SubjT<typeof schema, U>
 
-      type ExtUnd = ExtWith_SchemaParams_SubjT<{ optional: true }, U>
+    check<Actual, Expected>()
+    check<Expected, Actual>()
+  })
 
-      check<U | undefined, ExtUnd>
-      // @ts-expect-error U != U | undefined
-      check<U, ExtUnd>
+  it('Optional + nullable + branded', () => {
+    const schema = {
+      optional: true,
+      nullable: true,
+      brand: ['x', 'y'],
+    } as const
 
-      type ExtUndNull = ExtWith_SchemaParams_SubjT<
-        { optional: true; nullable: true },
-        U
-      >
+    type Expected = (U & { __x: 'y' }) | null | undefined
+    type Actual = ExtWith_SchemaParams_SubjT<typeof schema, U>
 
-      check<U | undefined | null, ExtUndNull>
-      // @ts-expect-error U | undefined != U | undefined | null
-      check<U | undefined, ExtUndNull>
-
-      type ExtUndNullBrand = ExtWith_SchemaParams_SubjT<
-        { optional: true; nullable: true; brand: Readonly<['x', 'y']> },
-        U
-      >
-
-      check<undefined | null | (U & { __x: 'y' }), ExtUndNullBrand>
-      // @ts-expect-error undefined | null != undefined | null | (U & { __x: 'y' }
-      check<undefined | null, ExtUndNullBrand>
-    })
-
-    it('ExtWith_SchemaParams_SubjT<T, U>: brand -> null -> undefined', () => {
-      type NotExt = ExtWith_SchemaParams_SubjT<null, U>
-
-      check<U, NotExt>
-      // @ts-expect-error always
-      check<never, NotExt>
-
-      type ExtBrand = ExtWith_SchemaParams_SubjT<
-        { brand: Readonly<['x', 'y']> },
-        U
-      >
-
-      check<U & { __x: 'y' }, ExtBrand>
-      // @ts-expect-error always
-      check<never, ExtBrand>
-
-      type ExtBrandNull = ExtWith_SchemaParams_SubjT<
-        { brand: Readonly<['x', 'y']>; nullable: true },
-        U
-      >
-
-      check<(U & { __x: 'y' }) | null, ExtBrandNull>
-      // @ts-expect-error U & { __x: 'y' } != U & { __x: 'y' }) | null
-      check<U & { __x: 'y' }, ExtBrandNull>
-
-      type ExtBrandNullUndef = ExtWith_SchemaParams_SubjT<
-        { brand: Readonly<['x', 'y']>; nullable: true; optional: true },
-        U
-      >
-
-      check<(U & { __x: 'y' }) | null | undefined, ExtBrandNullUndef>
-      // @ts-expect-error U & { __x: 'y' } | null != U & { __x: 'y' }) | null | undefined
-      check<(U & { __x: 'y' }) | null, ExtBrandNullUndef>
-    })
+    check<Actual, Expected>()
+    check<Expected, Actual>()
   })
 })
